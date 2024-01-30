d04cc73ec2ef205d0a689d420f3256dad8d857d7
dumster <b.abduvaliyev@igenius.uz>
2024-01-30 15:56:48 +0500

    refactor code

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 import axios from 'axios';
 
+
 class AuthModule {
-    constructor(baseURL) {
+    constructor (baseURL) {
         this.baseURL = baseURL;
     }
-    async register(body) {
+
+    async register (body) {
         try {
-            const response = await axios.post(`${this.baseURL}/auth-back/api/v2/register`, body, {
+            const response = await axios.post(`${ this.baseURL }/auth-back/api/v2/register`, body, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
             });
             return response.data;
         } catch (error) {
-           return error
-        }
-    }
-
-    async confirmEmail(body) {
-        try {
-            const response = await axios.post(`${this.baseURL}/auth-back/api/v2/confirm-email`, body);
-            return response.data;
-        } catch (error) {
             return error
         }
     }
-    async sendConfirmCode(body) {
+
+    async confirmEmail (body) {
         try {
-            const response = await axios.post(`${this.baseURL}/auth-back/api/v2/sendConfirmCode`, body);
+            const response = await axios.post(`${ this.baseURL }/auth-back/api/v2/confirm-email`, body);
             return response.data;
         } catch (error) {
             return error
         }
     }
 
-    async login(body) {
+    async sendConfirmCode (body) {
         try {
-            const response = await axios.post(`${this.baseURL}/auth-back/api/v2/login`, body);
+            const response = await axios.post(`${ this.baseURL }/auth-back/api/v2/sendConfirmCode`, body);
             return response.data;
         } catch (error) {
             return error
         }
     }
 
-    async checkAuthorization(token) {
+    async login (body) {
         try {
-            const response = await axios.get(`${this.baseURL}/auth-back/api/v2/protected-route`, {
+            const response = await axios.post(`${ this.baseURL }/auth-back/api/v2/login`, body);
+            return response.data;
+        } catch (error) {
+            return error
+        }
+    }
+
+    async checkAuthorization (token) {
+        try {
+            const response = await axios.get(`${ this.baseURL }/auth-back/api/v2/protected-route`, {
                 headers: { Authorization: token }
             });
             return response.data;
@@ -54,22 +57,15 @@ class AuthModule {
         }
     }
 
-    async getMe(body) {
+    async getMe (body) {
         try {
-            const response = await axios.post(`${this.baseURL}/auth-back/api/v2/getUserInfo`, body);
+            const response = await axios.post(`${ this.baseURL }/auth-back/api/v2/getUserInfo`, body);
             return response.data;
         } catch (error) {
             return error
         }
     }
 }
-
-/*const auth = new AuthModule('https://helloam');
-const data = {
-    email: 'fewfew@fewfew.few',
-};
-
-auth.sendConfirmCode(data);*/
 
 export default AuthModule;
 
